7ef1b27911c64e56f2e36a2a7530be9d2ef50601
dtertin <165623111+dtertin@users.noreply.github.com>
2024-04-15 09:22:50 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,84 @@
+from pygame import *
+from random import randint
+
+
+
+
+
+img_back = "orig.webp"
+img_wall = "wall.png"
+img_ball = "ball.png"
+
+
+
+
+class GameSprite(sprite.Sprite):
+ #конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        #вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+
+
+        #каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+
+        #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    #метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+
+
+class Player(GameSprite):
+   #метод для управления спрайтом стрелками клавиатуры
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.x > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.x < win_width - 5:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.x < win_width - 5:
+            self.rect.y += self.speed
+
+
+
+
+win_width = 700
+win_height = 500
+display.set_caption("Ping_Pong")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
+
+
+wall = Player(img_wall, 5, win_height - 100, 80, 100, 10)
+wall2 = Player(img_wall, 650, win_height - 100, 80, 100, 10)
+
+clock = time.Clock()
+FPS = 60
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+    window.blit(background,(0, 0))
+    wall.reset()
+    wall.update_l()
+
+    wall2.reset()
+    wall2.update_r()
+
+    display.update()
+    clock.tick(FPS)
